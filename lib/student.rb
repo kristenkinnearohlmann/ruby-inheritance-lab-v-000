9017b5b6bc871c5cc1626ebf1c6b6db5f29743e9
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-08-06 01:49:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,22 +3,18 @@ require "pry"
 
 class Student < User
 
-  # attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name
 
   def initialize()
     @knowledge = []
   end
 
   def learn(knowledge)
-    binding.pry
     @knowledge << knowledge
-    binding.pry
   end
 
   def knowledge
-    binding.pry
     @knowledge
-    binding.pry
   end
 
 end
